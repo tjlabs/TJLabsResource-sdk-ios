32eaeb79ada5e5e12b3cbb70027115a3d41b6b2b
Leo <dev@tjlabscorp.com>
2025-01-22 12:48:15 +0900

    feat: ResourceModel
    model for resources

--- TJLabsResource/ResourceModel.swift
+++ TJLabsResource/ResourceModel.swift
@@ -1,7 +1,54 @@
-//
-//  ResourceModel.swift
-//  TJLabsResource
-//
-//  Created by 신동현 on 1/21/25.
-//
 
+import Foundation
+
+public enum ResourceRegion {
+    case KOREA, US, CANADA
+}
+
+public struct PathPixelData {
+    public var roadType: [Int] = []
+    public var nodeNumber: [Int] = []
+    public var road: [[Double]] = [[]]
+    public var roadMinMax: [Double] = []
+    public var roadScale: [Double] = []
+    public var roadHeading: [String] = []
+    
+    public init(roadType: [Int], nodeNumber: [Int], road: [[Double]], roadMinMax:[Double], roadScale: [Double], roadHeading: [String]) {
+        self.roadType = roadType
+        self.nodeNumber = nodeNumber
+        self.road = road
+        self.roadMinMax = roadMinMax
+        self.roadScale = roadScale
+        self.roadHeading = roadHeading
+    }
+}
+
+public struct PathPixelDataIsLoaded {
+    public var isLoaded: Bool = false
+    public var URL: String = ""
+    
+    public init(isLoaded: Bool, URL: String) {
+        self.isLoaded = isLoaded
+        self.URL = URL
+    }
+}
+
+struct InputSector: Codable {
+    var sector_id: Int = 0
+    var operating_system: String = "iOS"
+    
+    init(sector_id: Int, operating_system: String) {
+        self.sector_id = sector_id
+        self.operating_system = operating_system
+    }
+}
+
+struct PathPixel: Codable {
+    let building_name: String
+    let level_name: String
+    let url: String
+}
+
+struct OutputPathPixel: Codable {
+    let path_pixel_list: [PathPixel]
+}
