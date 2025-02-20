c87feae42d928fae2f010b6e01f0a90856b21a20
Leo <dev@tjlabscorp.com>
2025-02-20 14:22:46 +0900

    feat: ResourecModel
    add structs for Entrance

--- TJLabsResource/ResourceModel.swift
+++ TJLabsResource/ResourceModel.swift
@@ -35,6 +35,44 @@ public struct PathPixelDataIsLoaded {
     }
 }
 
+public struct EntranceData {
+    public var number: Int = 0
+    public var networkStatus: Bool = false
+    public var velocityScale: Double = 0
+    public var innerWardId: String = ""
+    public var innerWardRssi: Double = 0
+    public var innerWardCoord: [Double] = []
+    
+    init(number: Int, networkStatus: Bool, velocityScale: Double, innerWardId: String, innerWardRssi: Double, innerWardCoord: [Double]) {
+        self.number = number
+        self.networkStatus = networkStatus
+        self.velocityScale = velocityScale
+        self.innerWardId = innerWardId
+        self.innerWardRssi = innerWardRssi
+        self.innerWardCoord = innerWardCoord
+    }
+}
+
+public struct EntranceRouteData {
+    public var routeLevel: [String] = []
+    public var route: [[Double]] = [[]]
+    
+    public init(routeLevel: [String], route: [[Double]]) {
+        self.routeLevel = routeLevel
+        self.route = route
+    }
+}
+
+public struct EntranceRouteDataIsLoaded {
+    public var isLoaded: Bool = false
+    public var URL: String = ""
+    
+    public init(isLoaded: Bool, URL: String) {
+        self.isLoaded = isLoaded
+        self.URL = URL
+    }
+}
+
 // MARK: - POST Input
 struct SectorIdInput: Codable {
     var sector_id: Int = 0
@@ -64,6 +102,33 @@ struct PathPixelOutput: Codable {
 
 struct PathPixelOutputList: Codable {
     let path_pixel_list: [PathPixelOutput]
+}
+
+// MARK: - Entrance
+struct EntranceRF: Codable {
+    let id: String
+    let rss: Double
+    let pos: [Double]
+    let direction: Double
+}
+
+struct Entrance: Codable {
+    let spot_number: Int
+    let outermost_ward_id: String
+    let scale: Double
+    let url: String
+    let network_status: Bool
+    let innermost_ward: EntranceRF
+}
+
+struct EntranceList: Codable {
+    let building_name: String
+    let level_name: String
+    let entrances: [Entrance]
+}
+
+struct EntranceOutputList: Codable {
+    let entrance_list: [EntranceList]
 }
 
 // MARK: - Building Level
